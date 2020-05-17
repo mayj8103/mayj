7029084ddcaeb213f92ea0c4b45cd02e60105fe5
mayj <271281476@qq.com>
2020-05-17 12:32:23 +0800

    create

---
+++ helloWorld/src/test/java/com/mayj/Test1.java
@@ -0,0 +1,17 @@
+package com.mayj;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		System.out.println("*************************");
+		System.out.println("test");
+		System.out.println("*************************");
+	}
+
+}
